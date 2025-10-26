19f881248f89aefa68f54e03f6655085865a0cf3
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2025-10-26 10:52:57 +0600

    added login page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,29 @@
+import { PlusIcon, UploadCloudIcon } from "lucide-react";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Dashboard page</h2>
+    <div className="">
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <p className="text-2xl font-me mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
+          Welcome,Joe Doe
+        </p>
+
+        <div className="flex gap-4">
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow=lg transition-all duration-300 cursor-pointer">
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-indigo-300 to-indigo-500 text-white rounded-full" />
+            <p className="text-sm group-hover:text-indigo-500 transition-all">
+              Create Resume
+            </p>
+          </button>
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow=lg transition-all duration-300 cursor-pointer">
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-purple-300 to-purple-500 text-white rounded-full" />
+            <p className="text-sm group-hover:text-indigo-500 transition-all">
+              Upload Existing
+            </p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <div>
-      <h1>layout page</h1>
-      <div>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
         <Outlet></Outlet>
       </div>
     </div>
